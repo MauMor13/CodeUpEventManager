717769a1e1ece1af3e037d6b849d873b03eb7630
Mauricio <mauri.f.mores@gmail.com>
2024-09-26 20:24:51 -0300

    ADD: create service and implementation

--- src/main/java/com/mindhub/event_manager/services/CustomerService.java
+++ src/main/java/com/mindhub/event_manager/services/CustomerService.java
@@ -1,4 +1,12 @@
 package com.mindhub.event_manager.services;
 
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
 public interface CustomerService {
+
+    ResponseEntity<?> getCustomers();
+
+    ResponseEntity<?> getCustomer(UUID id);
 }
--- src/main/java/com/mindhub/event_manager/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/mindhub/event_manager/services/impl/CustomerServiceImpl.java
@@ -1,4 +1,44 @@
 package com.mindhub.event_manager.services.impl;
 
-public class CustomerServiceImpl {
+import com.mindhub.event_manager.DTOs.CustomerDTO;
+import com.mindhub.event_manager.models.Customer;
+import com.mindhub.event_manager.repositories.CustomerRepository;
+import com.mindhub.event_manager.services.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public ResponseEntity<?> getCustomers() {
+
+        List<CustomerDTO> customers = customerRepository.findAll().stream().map(CustomerDTO::new).toList();
+
+        if (customers.isEmpty())
+            return new ResponseEntity<>("Were not found customers", HttpStatus.NOT_FOUND);
+
+        return  new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> getCustomer(UUID id) {
+        Optional<Customer> customerDTO = customerRepository.findById(id);
+        if (customerDTO.isEmpty())
+            return new ResponseEntity<>("Were not found customer", HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(new CustomerDTO(customerDTO.get()), HttpStatus.OK);
+    }
+
 }
