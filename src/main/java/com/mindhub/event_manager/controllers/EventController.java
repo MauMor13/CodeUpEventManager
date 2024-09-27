18455a119316a99b0efd30f17f426860ffec6947
Mauricio <mauri.f.mores@gmail.com>
2024-09-27 20:07:28 -0300

    REFACTOR: adding annotations

--- src/main/java/com/mindhub/event_manager/controllers/EventController.java
+++ src/main/java/com/mindhub/event_manager/controllers/EventController.java
@@ -1,4 +1,10 @@
 package com.mindhub.event_manager.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/event")
 public class EventController {
+
 }
--- src/main/java/com/mindhub/event_manager/controllers/LocationController.java
+++ src/main/java/com/mindhub/event_manager/controllers/LocationController.java
@@ -1,4 +1,9 @@
 package com.mindhub.event_manager.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/location")
 public class LocationController {
 }
