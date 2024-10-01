60f9c7261e279a93f5c7c62d735289573b1d3929
Mauricio <mauri.f.mores@gmail.com>
2024-10-01 10:38:48 -0300

    REFACTOR : inheritance implementation changes

--- src/main/java/com/mindhub/event_manager/models/Customer.java
+++ src/main/java/com/mindhub/event_manager/models/Customer.java
@@ -3,42 +3,23 @@ package com.mindhub.event_manager.models;
 import com.mindhub.event_manager.enums.CustomerGender;
 import com.mindhub.event_manager.enums.CustomerRol;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
-public class Customer {
+public class Customer extends Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Setter(AccessLevel.NONE)
-    private UUID customer_id;
-
-    private String name;
-
-    private String lastname;
-
-    private String email;
-
-    private boolean activated = true;
-
-    private String password;
 
     private byte age;
 
     @Enumerated(EnumType.STRING)
     private CustomerGender gender;
-
-    @Enumerated(EnumType.STRING)
-    private CustomerRol rol;
 
     @OneToMany (mappedBy = "customer")
     private Set<Event> events = new HashSet<>();
@@ -53,13 +34,9 @@ public class Customer {
     private Set<Reaction> reactions = new HashSet<>();
 
     public Customer(String name, String lastname, String email, String password, byte age, CustomerGender gender, CustomerRol rol) {
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-        this.password = password;
+        super(lastname, name, email, password, rol);
         this.age = age;
         this.gender = gender;
-        this.rol = rol;
     }
 
     public void addEvent(Event event){
