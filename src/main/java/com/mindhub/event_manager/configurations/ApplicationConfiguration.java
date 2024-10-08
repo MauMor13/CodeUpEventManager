c1f4614a9f8a57f3b8085ffc78382949988d241e
Mauricio <mauri.f.mores@gmail.com>
2024-10-08 20:59:31 -0300

    ADD: application configuration with user authentication and password encoding.

---
+++ src/main/java/com/mindhub/event_manager/configurations/ApplicationConfiguration.java
@@ -0,0 +1,42 @@
+package com.mindhub.event_manager.configurations;
+
+import com.mindhub.event_manager.services.PersonService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    private final PersonService personService;
+
+    public ApplicationConfiguration(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> personService.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
