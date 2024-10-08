7ea62dad3a0ed091ddbc9c985f3a22719872d674
Mauricio <mauri.f.mores@gmail.com>
2024-10-08 20:54:35 -0300

    ADD: create response the login

---
+++ src/main/java/com/mindhub/event_manager/models/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.mindhub.event_manager.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private long expiresIn;
+}
