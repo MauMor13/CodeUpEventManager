a7e1f9a31e74a936be5a4b6af6180cd30b226a73
Mauricio Mores <mauri.f.mores@gmail.com>
2024-11-05 21:41:10 -0300

    REFACTOR: create news implements methods

--- src/main/java/com/mindhub/event_manager/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/mindhub/event_manager/services/impl/CustomerServiceImpl.java
@@ -1,24 +1,53 @@
 package com.mindhub.event_manager.services.impl;
 
 import com.mindhub.event_manager.DTOs.CustomerDTO;
+import com.mindhub.event_manager.DTOs.PatchCustomerDTO;
+import com.mindhub.event_manager.DTOs.PersonLoginDTO;
 import com.mindhub.event_manager.models.Customer;
 import com.mindhub.event_manager.repositories.CustomerRepository;
 import com.mindhub.event_manager.services.CustomerService;
+import com.mindhub.event_manager.services.sendEmail.ServiceSendEmail;
+import jakarta.mail.MessagingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    private final ServiceSendEmail serviceSendEmail;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository,
+                               ServiceSendEmail serviceSendEmail,
+                               PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.serviceSendEmail = serviceSendEmail;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Customer findByEmail(String email) {
+        return customerRepository.findByEmail(email);
+    }
+
+    public boolean emailIsValid(String email) {
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     @Override
@@ -50,6 +79,62 @@ public class CustomerServiceImpl implements CustomerService {
         customer.get().setActivated(false);
         customerRepository.save(customer.get());
         return new ResponseEntity<>("Delete customer", HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> getCustomerAuthenticated() {
+
+        Authentication authenticationCustomer = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authenticationCustomer == null){
+            return new ResponseEntity<>("Customer not found", HttpStatus.BAD_REQUEST);
+        }
+
+        Customer customerAuth = this.findByEmail(authenticationCustomer.getName());
+        CustomerDTO customerDTO = new CustomerDTO(customerAuth);
+
+        return new ResponseEntity<>(customerDTO ,HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> registerCustomer(PersonLoginDTO newCustomer) {
+
+        if (!this.emailIsValid(newCustomer.getEmail())){
+            return new ResponseEntity<>("Your email is invalid", HttpStatus.BAD_REQUEST);
+        }
+        if(customerRepository.existsByEmail(newCustomer.getEmail())){
+            return new ResponseEntity<>("Email already registered", HttpStatus.CONFLICT);
+        }
+
+        Customer customer = new Customer(
+                passwordEncoder.encode(newCustomer.getPassword()),
+                newCustomer.getEmail()
+        );
+        customerRepository.save(customer);
+
+        try{
+            serviceSendEmail.sendCode(customer.getEmail());
+        }catch (MessagingException | UnsupportedEncodingException e){
+            return new ResponseEntity<>("The confirmation email could not be issued, please try again",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("\n" + "Correctly registered, please check the email to confirm",HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<?> patchAttributeCustomer(PatchCustomerDTO patchCustomerDTO) {
+
+        Authentication authenticationCustomer = SecurityContextHolder.getContext().getAuthentication();
+        if (authenticationCustomer == null){
+            return new ResponseEntity<>("Customer not found", HttpStatus.BAD_REQUEST);
+        }
+        Customer customerAuth = this.findByEmail(authenticationCustomer.getName());
+
+        customerAuth.setAge(patchCustomerDTO.age());
+        customerAuth.setName(patchCustomerDTO.name());
+        customerAuth.setLastname(patchCustomerDTO.lastname());
+        customerAuth.setGender(patchCustomerDTO.gender());
+
+        return  new ResponseEntity<>("Customer modification ok", HttpStatus.OK);
     }
 
 }
