13f5e650fb70b947a07204a121a50c3602cf76ae
Mauricio Mores <mauri.f.mores@gmail.com>
2024-11-05 21:40:42 -0300

    REFACTOR: create news methods

--- src/main/java/com/mindhub/event_manager/services/CustomerService.java
+++ src/main/java/com/mindhub/event_manager/services/CustomerService.java
@@ -1,14 +1,24 @@
 package com.mindhub.event_manager.services;
 
+import com.mindhub.event_manager.DTOs.PatchCustomerDTO;
+import com.mindhub.event_manager.DTOs.PersonLoginDTO;
+import com.mindhub.event_manager.models.Customer;
 import org.springframework.http.ResponseEntity;
-
 import java.util.UUID;
 
 public interface CustomerService {
+
+    Customer findByEmail(String email);
 
     ResponseEntity<?> getCustomers();
 
     ResponseEntity<?> getCustomer(UUID id);
 
     ResponseEntity<?> deleteCustomer(UUID id);
+
+    ResponseEntity<?> getCustomerAuthenticated();
+
+    ResponseEntity<?> registerCustomer(PersonLoginDTO newCustomer);
+
+    ResponseEntity<?> patchAttributeCustomer(PatchCustomerDTO patchCustomerDTO);
 }
